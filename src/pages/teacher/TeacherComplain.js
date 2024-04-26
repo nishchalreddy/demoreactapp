71e6368bddac79f705a8dcddcff395125b41300c
nishchalreddy <gvrnischalreddy77@gmail.com>
2024-04-27 04:11:19 +0530

    V1.0

---
+++ src/pages/teacher/TeacherComplain.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TeacherComplain = () => {
+  return (
+    <div>Counsellor Complain</div>
+  )
+}
+
+export default TeacherComplain
---
+++ src/redux/noticeRelated/noticeHandle.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import config from '../../config';
+import {
+    getRequest,
+    getSuccess,
+    getFailed,
+    getError
+} from './noticeSlice';
+const REACT_APP_BASE_URL = `${config.url}`;
+export const getAllNotices = (id, address) => async (dispatch) => {
+    dispatch(getRequest());
+
+    try {
+        const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/${address}List/${id}`);
+        if (result.data.message) {
+            dispatch(getFailed(result.data.message));
+        } else {
+            dispatch(getSuccess(result.data));
+        }
+    } catch (error) {
+        dispatch(getError(error));
+    }
+}
